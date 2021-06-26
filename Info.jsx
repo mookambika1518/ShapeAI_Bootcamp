71774ea14ddaee9f843991daf243c2cd8aa23c10
Mookambika S <86520159+mookambika1518@users.noreply.github.com>
2021-06-26 21:33:08 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <section>
+      <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p> This was an amazing bootcamp taken up Shaurya Sinha We covered everything from scratch including Javascript React.js,HTML and CSS</p>
+      </div>
+ 
+      <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p> This was an amazing bootcamp taken up Shaurya Sinha We covered everything from scratch including Javascript React.js,HTML and CSS</p>
+      </div>
+      </section>
+  ); 
+}
+export default Info;
